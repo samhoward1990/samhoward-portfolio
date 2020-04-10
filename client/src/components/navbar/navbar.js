ad9fa7e32ac5bffca2b170cdd446b9dc0e63920b
Sam Howard <samhoward1990@gmail.com>
2020-04-10 14:26:41 -0500

    added a background image to the App.css file and updated background and styling on the navbar component

--- client/src/components/navbar/navbar.js
+++ client/src/components/navbar/navbar.js
@@ -4,8 +4,8 @@ import './navbar.css';
 
 const NavbarMenu = () => (
     <Container>
-        <Navbar className="p-0">
-            <h1 className="styled-header p-3 m-0">Sam Howard</h1>
+        <Navbar bg="white" className="pt-0 pb-0">
+            <h1 className="styled-header p-3 mb-0 ml-5">Sam Howard</h1>
             <Nav className="ml-auto">
                 <Nav.Link className="border-right">About Me</Nav.Link>
                 <Nav.Link className="border-right">Portfolio</Nav.Link>
