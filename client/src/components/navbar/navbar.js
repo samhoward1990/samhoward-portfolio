df7c0f7f22423febdac89dd3cb76bf26d57e0007
Sam Howard <samhoward1990@gmail.com>
2020-04-11 12:02:45 -0500

    updated conflicting classes in navbar component

--- client/src/components/navbar/navbar.js
+++ client/src/components/navbar/navbar.js
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 const NavbarMenu = () => (
     <Container>
         <Navbar bg="white" className="pt-0 pb-0">
-            <h1 className="styled-header p-3 mb-0 ml-5">Sam Howard</h1>
+            <h1 className="styled-nav-header p-3 mb-0 ml-5">Sam Howard</h1>
             <Nav className="ml-auto">
                 <Link to="/" className="nav-link border-right">About Me</Link>
                 <Link to="/portfolio" className="nav-link border-right">Portfolio</Link>
