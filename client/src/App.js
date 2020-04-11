2b77a5555581f744b3571634167e6d88af04e265
Sam Howard <samhoward1990@gmail.com>
2020-04-11 14:32:53 -0500

    Added an about page with content

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import NavbarMenu from './components/navbar/navbar';
+import About from './components/about/about';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <NavbarMenu />
+        <Switch>
+          <Route exact path="/">
+            <About />
+          </Route>
+        </Switch>
       </div>
     </Router>
   );
---
+++ client/src/components/about/about.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Container, Row, Col, Image } from 'react-bootstrap';
+import './about.css';
+
+import Sam from '../../images/sam.jpg';
+
+const About = () => (
+    <Container className="mt-3">
+        <div className="bg-white">
+            <Row>
+                <Col xs="12">
+                    <h2 className="styled-header border-bottom p-3 ml-5 mb-0">About Me</h2>
+                </Col>
+            </Row>
+            <Row>
+                <Col xs="3">
+                    <Image className="m-5" src={Sam} alt="Sam Howard" fluid />
+                </Col>
+                <Col xs="9">
+                    <p className="ml-5 mt-5 mr-5 ">I am a musician, teacher and now an aspiring web developer in Dallas, Texas. Musically I enjoy playing jazz and blues, as well as writing in many styles. I have been teaching for over 5 years, and have taught students of all ages at several studios. I have played in several bands in the Dallas/Fort Worth area, and enjoy collaboration.</p>
+                    <p className="ml-5 mr-5">As a developer I hope to bring creative and logical solutions to my workplace. I also would like to bring my spirit of teamwork and collaboration that I have learned from working in bands to the tech field. As an active learner I am looking forward to learning more everyday throughout my career.</p>
+                </Col>
+            </Row>
+        </div>
+    </Container>
+);
+
+export default About;
