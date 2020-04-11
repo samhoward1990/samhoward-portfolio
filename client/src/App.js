9351c383be0a4577f93de57ecbb84501a78ad080
Sam Howard <samhoward1990@gmail.com>
2020-04-11 11:52:36 -0500

    installed react-router-dom updated navbar component accordingly

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NavbarMenu from './components/navbar/navbar';
 
 function App() {
   return (
-    <div className="App">
-      <NavbarMenu />
-    </div>
+    <Router>
+      <div className="App">
+        <NavbarMenu />
+      </div>
+    </Router>
   );
 }
 
--- client/src/components/navbar/navbar.js
+++ client/src/components/navbar/navbar.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import { Container, Navbar, Nav } from 'react-bootstrap';
 import './navbar.css';
+import { Link } from 'react-router-dom';
 
 const NavbarMenu = () => (
     <Container>
         <Navbar bg="white" className="pt-0 pb-0">
             <h1 className="styled-header p-3 mb-0 ml-5">Sam Howard</h1>
             <Nav className="ml-auto">
-                <Nav.Link className="border-right">About Me</Nav.Link>
-                <Nav.Link className="border-right">Portfolio</Nav.Link>
-                <Nav.Link>Contact</Nav.Link>
+                <Link to="/" className="nav-link border-right">About Me</Link>
+                <Link to="/portfolio" className="nav-link border-right">Portfolio</Link>
+                <Link to="/contact" className="nav-link">Contact</Link>
             </Nav>
         </Navbar>
     </Container>
